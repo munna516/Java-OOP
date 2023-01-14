96ff9f5cf1c7292d1835455c14a8897c5e64b82a
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-01-14 09:37:24 +0600

    1st

---
+++ InstanceBlock.java
@@ -0,0 +1,31 @@
+class A {
+    int a, b;
+    String c;
+
+    A() {  // Constructor
+        a = 60;
+        b = 70;
+        System.out.println(a + " " + b);
+    }
+
+    { // Instance Block
+        a = 10;
+        b = 20;
+        System.out.println(a + " " + b);
+    }
+
+    void show() {   // Method
+        a = 40;
+        c = "Programming";
+        System.out.println(a + " " + c);
+    }
+}
+
+public class InstanceBlock {
+    public static void main(String[] args) {
+        A r = new A();
+        r.show();
+    }
+}
+
+// Intance variable execute first .
