c7ec9d12f53821ee74a9d28c62bb0d70333e6ffe
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-01-23 11:10:32 +0600

    new

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,19 +1,26 @@
 class A {
-    private int x;
+    private int x; // data hiding
+    private String s;
 
-    public void setvalue(int x) {
+    public void setvalue(int x, String s) {  // setter method
         this.x = x;
+        this.s = s;
     }
 
-    public int getvalue() {
+    public int getvalue() {  //getter method
         return ++x;
+    }
+
+    public String getvalues() {  //getter method
+        return s;
     }
 }
 
 public class Encapsulation {
     public static void main(String[] args) {
         A r = new A();
-        r.setvalue(19);
+        r.setvalue(19, "Munna");
         System.out.println(r.getvalue());
+        System.out.println(r.getvalues());
     }
 }
