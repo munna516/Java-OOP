7e32ccd6a10b3a7c03a641b2d0e82dff056efd7a
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-01-26 18:12:08 +0600

    new

---
+++ AbstruceClass.java
@@ -0,0 +1,33 @@
+abstract class Animal {
+    Animal() {
+        System.out.println("This is our Animal Class......");
+    }
+
+    public abstract void sound();
+
+}
+
+class Dog extends Animal {
+    Dog() {
+        super();
+    }
+
+    public void sound() {
+        System.out.println("The dog is barking.");
+    }
+}
+
+class Lion extends Animal {
+    public void sound() {
+        System.out.println("The Lion is Roar.");
+    }
+}
+
+public class AbstruceClass {
+    public static void main(String[] args) {
+        Dog r = new Dog();
+        r.sound();
+        Lion l = new Lion();
+        l.sound();
+    }
+}
